814d07ddc678d584ae7b4f40a42392e821404edf
nthduc <nguyenthaiduc0212@gmail.com>
2023-06-28 22:27:50 +0700

    1 LocalTransport

---
+++ README.md
@@ -0,0 +1,10 @@
+# Create Blockchain Layer 1 with Go
+
+Network
+Tx, Block
+Keypair (sha256, P256)
+Blockchain
+VM
+
+1. LocalTransport
+2. Server
---
+++ network/local_transport.go
@@ -0,0 +1,53 @@
+package network
+
+import (
+	"fmt"
+	"sync"
+)
+
+type LocalTransport struct {
+	addr      NetAddr
+	consumeCh chan RPC
+	peers     map[NetAddr]*LocalTransport
+	lock      sync.RWMutex
+}
+
+func NewLocalTransport(addr NetAddr) *LocalTransport {
+	return &LocalTransport{
+		addr:      addr,
+		consumeCh: make(chan RPC, 1024),
+		peers:     make(map[NetAddr]*LocalTransport),
+	}
+}
+
+func (tr *LocalTransport) Addr() NetAddr {
+	return tr.addr
+}
+
+func (tr *LocalTransport) Consume() <-chan RPC {
+	return tr.consumeCh
+}
+
+func (tr *LocalTransport) Connect(trb *LocalTransport) error {
+	tr.lock.Lock()
+	defer tr.lock.Unlock()
+
+	tr.peers[trb.Addr()] = trb
+	return nil
+}
+
+func (tr *LocalTransport) sendMessage(to NetAddr, payload []byte) error {
+	if tr.addr == to {
+		return fmt.Errorf("Could not send message to yourself!")
+	}
+	peer, ok := tr.peers[to]
+	if !ok {
+		return fmt.Errorf("%s: Could not send message to %s", tr.addr, to)
+	}
+
+	peer.consumeCh <- RPC{
+		From:    tr.addr,
+		Payload: payload,
+	}
+	return nil
+}
